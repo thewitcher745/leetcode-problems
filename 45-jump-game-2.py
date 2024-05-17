a38832a6228c727f64bbb514071a2f380d93cd46
thewitcher745 <thewitcher745@gmail.com>
2024-05-17 23:45:27 +0330

    FEAT: 45

---
+++ 45-jump-game-2.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        ans = 0
+        end = 0
+        farthest = 0
+
+        # Implicit BFS
+        for i in range(len(nums) - 1):
+            farthest = max(farthest, i + nums[i])
+            if farthest >= len(nums) - 1:
+                ans += 1
+                break
+            if i == end:  # Visited all the items on the current level
+                ans += 1  # Increment the level
+                end = farthest  # Make the queue size for the next level
+
+        return ans
+
+
+solution = Solution()
+nums = [2, 1]
+print(solution.jump(nums))
