090b2281b1e4e69eac96957e1fcbba14f7c396a7
thewitcher745 <thewitcher745@gmail.com>
2024-05-17 22:43:46 +0330

    FEAT: 1325

---
+++ 1325-delete-leaves-with-value.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        root.left = self.removeLeafNodes(root.left, target)
+        root.right = self.removeLeafNodes(root.right, target)
+
+        if root.left is None and root.right is None:
+            if root.val == target:
+                return None
+
+        return root
